b6b405f8efed64d4b141717fc22bffad071c1685
azen <aidenzich0221@gmail.com>
2022-06-28 13:03:03 +0800

    Refactoring with element concepts

---
+++ js/components/footer.js
@@ -0,0 +1,16 @@
+const footer = `
+<footer id="footer" class="footer">
+    <div class="main-footer text-center">
+    <div class="footer-copyright">
+        <div style="width: 100%;  color: dimgrey; font-style: italic; 
+            font-family:Verdana;
+            ">
+            <p style="width: 100%; text-align: center;">©2022 The 26<sub style="position: relative;bottom: 0px;">th</sub> International Conference on Technologies and Applications of Artificial Intelligence.</p>
+            <p><i class="fa fa-envelope"></i> taai2022.cyut@gmail.com</p>
+        </div>
+    </div>
+    </div>
+</footer>
+`
+
+document.write(footer);
---
+++ js/components/header.js
@@ -0,0 +1,14 @@
+const header = `<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Welcome to the official website of the 26th International Conference on Technologies and Applications of Artificial Intelligence (TAAI 2022)">
+    <meta name="keywords" content="TAAI2022, TAAI, Artificial Intelligence, Conference, TAAI 2022">
+    <title>TAAI 2022</title>
+    <link href="css/bootstrap-4.4.1.css" rel="stylesheet">
+    <link rel=stylesheet type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="js/custom.js"></script>
+    </head>`
+
+document.write(header);
