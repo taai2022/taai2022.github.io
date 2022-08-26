f23205a080426984d2ba3fbba5de4137d1174cd0
hykao <hykao@mail.ncku.edu.tw>
2022-08-26 09:44:59 +0800

    Update banner.js

--- js/components/banner.js
+++ js/components/banner.js
@@ -1,5 +1,5 @@
 const mobile_img = '73207.jpg'
-const img = '73208.jpg'
+const img = 'taai2022banner.jpg'
 const banner = `
 <div id="impadw" class="jumbotron-fluid" style="margin: auto; ">
     <img id="banim" src="images/${img}" class="img-fluid">	
