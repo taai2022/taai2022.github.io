f479290e421e9dcddb2aa48f3da0dbe5a2485c14
hykao <hykao@mail.ncku.edu.tw>
2022-11-01 16:16:46 +0800

    Update footer.js

--- js/components/footer.js
+++ js/components/footer.js
@@ -41,10 +41,8 @@ const footer = `
 <footer class="text-center text-lg-start bg-light text-muted">         
     <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
     ©2022 The 27th International Conference on Technologies and Applications of Artificial Intelligence.            
-    </div>
     <br>
-     <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
-     Contact Mail: ikm.ncku@gmail.com
+    Contact: ikm.ncku@gmail.com
      </div>
 </footer>
 
