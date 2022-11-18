62716f4fbe21386b91124b3a25f714a9d115874b
roselee777 <117155449+roselee777@users.noreply.github.com>
2022-11-18 21:05:09 +0800

    Update footer.js

--- js/components/footer.js
+++ js/components/footer.js
@@ -1,5 +1,5 @@
 const footer = `
-<br><br>
+<br/><br/>
 <div id="footer_info_w" class="footer_info_w">
     <div class="footer_info">
     <div class="sp2_area">
@@ -12,9 +12,9 @@ const footer = `
     <div class="sp2_area">
         <strong class="col-lg-2 col-sm-2" style="line-height:50px;white-space: nowrap;align-items: center;display: flex;">CO-ORGANIZED BY</strong>
         <ul class="col-lg-10 col-sm-10">
-            <li><a href="https://www.iis.sinica.edu.tw/" target="_blank"><img src="images/agencies/iis.png" title=""></a></li><br> 
-            <li><a href="https://www.citi.sinica.edu.tw/" target="_blank"><img src="images/agencies/CITI.jpeg" title=""></a></li><br> 
-            <li><a href="https://etop.org.tw/" target="_blank"><img src="images/agencies/etop.png" title=""></a></li><br> 
+            <li><a href="https://www.iis.sinica.edu.tw/" target="_blank"><img src="images/agencies/iis.png" title=""></a></li><br/> 
+            <li><a href="https://www.citi.sinica.edu.tw/" target="_blank"><img src="images/agencies/CITI.jpeg" title=""></a></li><br/> 
+            <li><a href="https://etop.org.tw/" target="_blank"><img src="images/agencies/etop.png" title=""></a></li><br/> 
         </ul>
     </div>
     <div class="sp2_area pb30">
@@ -27,16 +27,18 @@ const footer = `
         <strong class="col-lg-2 col-sm-2" style="line-height:50px;white-space: nowrap;align-items: center;display: flex;">DIAMOND SPONSORS</strong>
         <ul class="col-lg-10 col-sm-10">
             <li><a href="https://www.google.com/" target="_blank"><img src="images/agencies/googlelogo.png"></a></li>
-			<li><a href="https://aidea-web.tw/" target="_blank"><img src="images/agencies/ITRI.png" title=""></a></li><br>
+			<li><a href="https://aidea-web.tw/" target="_blank"><img src="images/agencies/ITRI.png" title=""></a></li><br/>
         </ul>
     </div>
     <div class="sp2_area pb30">
         <strong class="col-lg-2 col-sm-2" style="line-height:50px;white-space: nowrap;align-items: center;display: flex;">GOLD SPONSORS</strong>
         <ul class="col-lg-10 col-sm-10">
-            <li><a href="https://www.esunbank.com.tw/bank/personal" target="_blank"><img src="images/agencies/esan.png"></a></li><br>
-            <li><a href="https://www.wavenet.com.tw/" target="_blank"><img src="images/agencies/wavenet.png"></a></li><br>
-            <li><a href="https://www.kdanmobile.com/zh-tw?utm_source=PR&utm_campaign=PR_TAAI_sponsor2021&utm_medium=TAAI" target="_blank"><img src="images/agencies/kdan_logoa_4c_rgb_0.png"></a></li><br>
-			<li><a href="https://tws.twcc.ai/" target="_blank"><img src="images/agencies/TWS-Logo.png"></a></li><br>
+            <li><a href="https://www.esunbank.com.tw/bank/personal" target="_blank"><img src="images/agencies/esan.png"></a></li><br/>
+            <li><a href="https://www.wavenet.com.tw/" target="_blank"><img src="images/agencies/wavenet.png"></a></li><br/>
+            <li><a href="https://www.kdanmobile.com/zh-tw?utm_source=PR&utm_campaign=PR_TAAI_sponsor2021&utm_medium=TAAI" target="_blank"><img src="images/agencies/kdan_logoa_4c_rgb_0.png"></a></li><br/>
+			<li><a href="https://tws.twcc.ai/" target="_blank"><img src="images/agencies/TWS-Logo.png"></a></li><br/>
+			<li><a href="https://tws.twcc.ai/" target="_blank"><img src="images/agencies/Sino.jpg"/></a></li><br/>
+			<li><a href="https://tws.twcc.ai/" target="_blank"><img src="images/agencies/Appier-logo_blue.png"/></a></li><br/>
         </ul>
     </div>
     </div>
@@ -44,7 +46,7 @@ const footer = `
 <footer class="text-center text-lg-start bg-light text-muted">         
     <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
     ©2022 The 27th International Conference on Technologies and Applications of Artificial Intelligence.            
-    <br>
+    <br/>
     Contact: ikm.ncku@gmail.com
      </div>
 </footer>
