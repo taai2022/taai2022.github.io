483d54aa0eb18f15f678fcc04fe42bc1a95bd645
roselee777 <117155449+roselee777@users.noreply.github.com>
2022-11-20 19:36:02 +0800

    Update footer.js

--- js/components/footer.js
+++ js/components/footer.js
@@ -3,36 +3,36 @@ const footer = `
 <div id="footer_info_w" class="footer_info_w">
     <div class="footer_info">
     <div class="sp2_area">
-        <strong class="col-lg-2" col-sm-2 style="line-height:50px;white-space: nowrap;align-items: center;display: flex;">ORGANIZED BY</strong>
-        <ul class="col-lg-10 col-sm-10">
-            <li><a href="http://www.ncku.edu.tw/" target="_blank"><img src="images/agencies/NCKU.jpg"></a></li>
+        <h5 class="col-lg-12 col-sm-12" style="line-height:50px;white-space: nowrap;align-items: center;display: flex; font-weight: 700;">ORGANIZED BY</h5>
+        <ul class="col-lg-12 col-sm-12">
+            <li><a href="http://www.ncku.edu.tw/" target="_blank"><img src="images/agencies/NCKU.jpg" style="max-height: 70px;"></a></li>
             <li><a href="http://www.taai.org.tw/" target="_blank"><img src="images/agencies/TAAI.png"></a></li>
         </ul>
     </div>
     <div class="sp2_area">
-        <strong class="col-lg-2 col-sm-2" style="line-height:50px;white-space: nowrap;align-items: center;display: flex;">CO-ORGANIZED BY</strong>
-        <ul class="col-lg-10 col-sm-10">
+        <h5 class="col-lg-12 col-sm-12" style="line-height:50px;white-space: nowrap;align-items: center;display: flex; font-weight: 700;">CO-ORGANIZED BY</h5>
+        <ul class="col-lg-12 col-sm-12">
             <li><a href="https://www.iis.sinica.edu.tw/" target="_blank"><img src="images/agencies/iis.png" title=""></a></li><br/> 
             <li><a href="https://www.citi.sinica.edu.tw/" target="_blank"><img src="images/agencies/CITI.jpeg" title=""></a></li><br/> 
             <li><a href="https://etop.org.tw/" target="_blank"><img src="images/agencies/etop.png" title=""></a></li><br/> 
         </ul>
     </div>
-    <div class="sp2_area pb30">
-        <strong class="col-lg-2 col-sm-2" style="line-height:50px;white-space: nowrap;align-items: center;display: flex;">ADVISED BY</strong>
-        <ul class="col-lg-10 col-sm-10">
+    <div class="sp2_area">
+        <h5 class="col-lg-12 col-sm-12" style="line-height:50px;white-space: nowrap;align-items: center;display: flex; font-weight: 700;">ADVISED BY</h5>
+        <ul class="col-lg-12 col-sm-12">
             <li><a href="https://www.nstc.gov.tw//" target="_blank"><img src="images/agencies/NSTC.png"></a></li>
         </ul>
     </div>
-    <div class="sp2_area pb30">
-        <strong class="col-lg-2 col-sm-2" style="line-height:50px;white-space: nowrap;align-items: center;display: flex;">DIAMOND SPONSORS</strong>
-        <ul class="col-lg-10 col-sm-10">
+    <div class="sp2_area">
+        <h5 class="col-lg-12 col-sm-12" style="line-height:50px;white-space: nowrap;align-items: center;display: flex; font-weight: 700;">DIAMOND SPONSORS</h5>
+        <ul class="col-lg-12 col-sm-12">
             <li><a href="https://www.google.com/" target="_blank"><img src="images/agencies/googlelogo.png"></a></li>
 			<li><a href="https://aidea-web.tw/" target="_blank"><img src="images/agencies/ITRI.png" title=""></a></li><br/>
         </ul>
     </div>
-    <div class="sp2_area pb30">
-        <strong class="col-lg-2 col-sm-2" style="line-height:50px;white-space: nowrap;align-items: center;display: flex;">GOLD SPONSORS</strong>
-        <ul class="col-lg-10 col-sm-10">
+    <div class="sp2_area">
+        <h5 class="col-lg-12 col-sm-12" style="line-height:50px;white-space: nowrap;align-items: center;display: flex; font-weight: 700;">GOLD SPONSORS</h5>
+        <ul class="col-lg-12 col-sm-12">
             <li><a href="https://www.esunbank.com.tw/bank/personal" target="_blank"><img src="images/agencies/esan.png"></a></li><br/>
             <li><a href="https://www.wavenet.com.tw/" target="_blank"><img src="images/agencies/wavenet.png"></a></li><br/>
             <li><a href="https://www.kdanmobile.com/zh-tw?utm_source=PR&utm_campaign=PR_TAAI_sponsor2021&utm_medium=TAAI" target="_blank"><img src="images/agencies/kdan_logoa_4c_rgb_0.png"></a></li><br/>
